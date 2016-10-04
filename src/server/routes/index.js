143918a10cb0fa926945797c0b14c7ab8a0a90ee
Andrey Krypskii <andreykrupskii@gmail.com>
2016-10-04 23:07:24 +0300

    add async for create index component

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,14 +1,81 @@
-import renderHTML from './../libs/renderHTML';
 import logger from './../libs/log';
+import waterfall from 'async/waterfall';
 
+import React from 'react';
+import ReactDom from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import rootReducer from './../../client/reducers';
+import App from './../../client/components/App.component';
 
 const log = logger(module);
 
 module.exports = function(req, res, next) {
-	const redux = require('./../libs/getComponentHTML');
-	const componentHtml = redux.componentHtml;
-	const preloadedState = redux.preloadedState; 
+	waterfall(
+		[
+			isAuth, 
+			getState, 
+			createReactComponent, 
+			renderHTML
+		], 
+		function(err, result){
+			if(err) {
+				log.error(err);
+				result = 'lol'
+			}
+			res.end(result);
+		}
+	)
 
-	const elementHTML = renderHTML(componentHtml, preloadedState);
-	res.end(elementHTML);
+
+function isAuth(callback){
+	const haveSession = req.session.isAuth;
+	callback(null, haveSession);
+}
+
+function getState(haveSession, callback){
+	const state = {
+		name: 'Server Charly'
+	}
+	callback(null, state);
+}
+
+function createReactComponent(state, callback){
+	const	store	=	createStore(rootReducer, state); 
+	const preloadedState = store.getState();
+
+	const componentHtml = ReactDom.renderToString(
+			<Provider	store={store}>				
+				<App/>		
+			</Provider>
+		);
+
+	
+	callback(null, componentHtml, preloadedState)
+}
+
+function renderHTML(componentHtml, preloadedState, callback){
+	const assetUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:9999/public' : '.';
+	const indexHtml = `
+		<!DOCTYPE html>
+			<html>
+			<head>
+					<meta charset="utf-8">
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+					<title>Hello React</title>
+					<link rel="stylesheet" href="${assetUrl}/assets/styles.css">
+			</head>
+			<body>
+				<div id="react-view">${componentHtml}</div>
+				<script>
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
+        </script>
+				<script type="application/javascript" src="${assetUrl}/assets/bundle.js"></script>
+			</body>
+		</html>
+	`;
+	callback(null, indexHtml);
+}
+
 };
